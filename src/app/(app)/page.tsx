d74b6b3bd9ef51037db260e8132ad48ff1dfbb00
abhinav7895 <abhinavay2003@gmail.com>
2024-09-08 16:45:54 +0530

    Refactor page.tsx to update background colors and improve code readability

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -31,11 +31,11 @@ export default function Home() {
     
       
       <Navbar />
-      <div className="bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px]">
+      <div className="bg-neutral-950 bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px]">
       <MaxWidthWrapper className="pt-[90px] sm:pt-[120px] px-3 ">
         <RadialGradient className="-z-10" />
         <div className="text-center mt-5 px-2.5 sm:mt-10  mx-auto">
-          <h1 className={" text-2xl sm:text-3xl md:text-5xl  text-balance mb-8 " + fraunces.className}>Generate Interview Questions <br />
+          <h1 className={" text-2xl sm:text-3xl md:text-5xl  text-balance mb-8 text-neutral-200 " + fraunces.className}>Generate Interview Questions <br />
             <span className="text-green-500">10x Faster</span> with <span className="text-green-500">AI-Powered Precision</span></h1>
           <p className="text-balance text-base text-neutral-300 sm:text-xl mb-8">
             Get AI-Generated Interview Questions Tailored to
@@ -63,7 +63,7 @@ export default function Home() {
           </div>
 
           <div className="flex flex-col space-y-8 sm:space-y-16 mt-[60px] sm:mt-[100px]">
-            <h2 className={"text-2xl sm:text-3xl md:text-4xl text-start " + fraunces.className}>
+            <h2 className={"text-2xl sm:text-3xl md:text-4xl text-start text-neutral-200 " + fraunces.className}>
               Key <span className="text-green-500">Benefits</span>  of  <br />
               <span className="text-green-500">AI-Powered Interview Preparation  </span>
 
@@ -88,7 +88,7 @@ export default function Home() {
           </div>
 
           <div className="mt-[100px] space-y-8 sm:space-y-16">
-            <h2 className={"text-2xl sm:text-3xl md:text-4xl text-start " + fraunces.className}>
+            <h2 className={"text-2xl sm:text-3xl md:text-4xl text-start text-neutral-200 " + fraunces.className}>
               <span className="text-green-500">Generate</span> Questions <br />
               and <span className="text-green-500">Ace Your Interviews  </span>
 
@@ -151,10 +151,10 @@ export default function Home() {
 
         </div>
 
-        <div className="w-full p-5 rounded-2xl border h-[400px] my-[100px] bg-opacity-35 bg-[linear-gradient(to_right,#052e1650_1px,transparent_1px),linear-gradient(to_bottom,#052e1650_1px,transparent_1px)] bg-[size:24px_24px] flex justify-center items-center">
+        <div className="w-full p-5 rounded-2xl border border-neutral-800 h-[400px] my-[100px] bg-opacity-35 bg-[linear-gradient(to_right,#052e1650_1px,transparent_1px),linear-gradient(to_bottom,#052e1650_1px,transparent_1px)] bg-[size:24px_24px] flex justify-center items-center">
            <div className="flex flex-col items-center gap-6">
            <AnimatedSparkle animate={true} size={40} color='white' />
-           <p className={"text-2xl  font-light text-center " + fraunces.className}>Effortlessly Create Role-Specific Interview Questions with AI Assistance</p>
+           <p className={"text-2xl  font-light text-center text-neutral-200 " + fraunces.className}>Effortlessly Create Role-Specific Interview Questions with AI Assistance</p>
            <Link href={"/home"} className="relative w-fit inline-flex h-12 overflow-hidden rounded-full p-[1px] active:scale-90 transition-all">
               <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#22c55d_50%,#22c55d_100%)]" />
               <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-white-950 px-3 py-1 text-sm font-medium bg-gradient-to-r from-green-950 to-green-950 text-white backdrop-blur-3xl">
@@ -164,17 +164,17 @@ export default function Home() {
            </div>
         </div>
         
-        <div className="bg-gradient-to-l my-20 sm:[150px] from-transparent via-green-300 dark:via-neutral-300 to-transparent w-full h-[1px] " />
+        <div className="bg-gradient-to-l my-20 sm:[150px] from-transparent via-neutral-300 to-transparent w-full h-[1px] " />
         <footer className="  ">
           <div className="flex justify-between items-center">
-            <div className="flex items-center gap-1 px-2.5 sm:text-2xl">
+            <div className="flex items-center gap-1 px-2.5 sm:text-2xl text-neutral-300">
               <MdOutlineStreetview className='text-xl sm:text-3xl text-green-500' /> InterviewGenie
             </div>
-            <div className="flex gap-2 sm:gap-4 px-2.5 text-xl sm:text-3xl items-center">
+            <div className="flex text-neutral-300 gap-2 sm:gap-4 px-2.5 text-xl sm:text-3xl items-center">
               <Link href={"https://github.com/abhinav7895/interviewgenie"}><FaGithub /></Link> <Link href={"https://x.com/abhinavvay"}><FaXTwitter /></Link>
             </div>
           </div>
-          <div className="mb-[40px] mt-2 px-3 flex items-center gap-3 ">
+          <div className="pb-[40px] mt-2 px-3 flex items-center gap-3 ">
             <Link className="text-neutral-500 text-[13px] hover:text-neutral-400 transition-all" href={"https://www.interviewgenie.live/privacy"}>Privacy Policy</Link>
             <Link className="text-neutral-500 text-[13px] hover:text-neutral-400 transition-all" href={"https://www.interviewgenie.live/tos"}>Terms of Service</Link>
           </div>
